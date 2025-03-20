b96c5c3d40b32670aec28eeadd0440ea077be0b7
Wallyson Siqueira <manin.mp40@gmail.com>
2025-03-20 14:39:42 -0400

    Add files via upload
    tava muito facil, manda outra !

---
+++ ex1.py
@@ -0,0 +1,1 @@
+print("seja bem-vindo ao mundo python !")
---
+++ ex3.py
@@ -0,0 +1,10 @@
+num1=int(input("insira o primeiro numero"))
+num2=int(input("insira o segundo numero"))
+soma=num1+num2
+subtracao=num1-num2
+divisao=num1/num2
+multiplicacao=num1*num2
+print(f"(o resulado da soma é {soma})")
+print(f"(o resultado da subtracao é {subtracao})")
+print(F" o resultado da divisao é {divisao}")
+print(F"o resultado da multiplicacao é {multiplicacao}")
