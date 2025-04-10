6ed1c4eaef77507f556ea91526ede3ebfe5e42ef
Wallyson Siqueira <manin.mp40@gmail.com>
2025-04-10 14:17:35 -0400

    Add files via upload

--- ex14.py
+++ ex14.py
@@ -1,6 +1,6 @@
 nome_usuario=(input("escreva seu nome de usuario: "))
 senha=(input("digite sua senha: "))
-if nome_usuario == "Bia" and senha == "54321":
+if nome_usuario == "wallyson" and senha == "012345":
         print("Bem vindo")
 else:
         print("acesso negado")
